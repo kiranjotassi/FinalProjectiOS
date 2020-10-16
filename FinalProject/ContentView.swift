40dd304eb17adf77263b0529b28a79d63fc0e95d
Kiranjot Assi <assik@sheridancollege.ca>
2020-10-16 08:29:55 -0400

    Initial Commit

---
+++ FinalProject/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FinalProject
+//
+//  Created by Kiranjot Assi on 2020-10-16.
+//
+
+import SwiftUI
+
+//Main body of the app
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
