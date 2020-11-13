9bc376bbd1c5362b99c0d3851ad33781942dabfa
Kiranjot Assi <assik@sheridancollege.ca>
2020-11-13 10:37:21 -0500

    Added Pods, edited App and Scene Delegate

--- FinalProject/SceneDelegate.swift
+++ FinalProject/SceneDelegate.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 import SwiftUI
+import FirebaseFirestore
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    let db = Firestore.firestore()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FinalProject' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FinalProject
+  
+  #Firebase
+  pod 'Firebase'
+  
+  #Cloud Firestore
+  pod 'Firebase/Firestore'
+  
+  #Swift extensions
+  pod 'FirebaseFirestoreSwift'
+
+end
