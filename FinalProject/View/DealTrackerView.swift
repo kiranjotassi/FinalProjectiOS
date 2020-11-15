8560a0d5b9b39db6afc98f53205a7927427d0193
Patrick Ouellette <patrick.ouellette@hotmail.com>
2020-11-15 18:43:36 -0500

    Deal Tracker page UI done

--- FinalProject/View/DealTrackerView.swift
+++ FinalProject/View/DealTrackerView.swift
@@ -9,7 +9,57 @@ import SwiftUI
 
 struct DealTrackerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                Text("Deals in your Area")
+                    .padding()
+                    .font(.title)
+                    .frame(alignment: .leading)
+                
+                List{
+                    HStack{
+                        //https://sfsymbols.com/
+                        Image(systemName: "purchased.circle")
+                            .padding()
+                            .font(.system(size: 50))
+                        VStack(alignment: .leading){
+                            Text("Store Name: (name)")
+                            Text("Deal Item & Price or if Store wide sale")
+                            Text("Location: (insert location)")
+                            Button(action: {
+                                print("Location Clicked")
+                            }){
+                                Text("View Location on Map")
+                                    .foregroundColor(Color.blue)
+                            }
+                            .buttonStyle(BorderlessButtonStyle())
+                        }
+                    }
+                    
+                    HStack{
+                        //https://sfsymbols.com/
+                        Image(systemName: "purchased.circle")
+                            .padding()
+                            .font(.system(size: 50))
+                        VStack(alignment: .leading){
+                            Text("Store Name: (name)")
+                            Text("Deal Item & Price or if Store wide sale")
+                            Text("Location: (insert location)")
+                            Button(action: {
+                                print("Location Clicked")
+                            }){
+                                Text("View Location on Map")
+                                    .foregroundColor(Color.blue)
+                            }
+                            .buttonStyle(BorderlessButtonStyle())
+                        }
+                    }
+                    
+                }//List
+            }//VStack
+            .navigationBarTitle("Available Deals", displayMode: .inline)
+            .navigationBarBackButtonHidden(false)
+        }//NavigationView
     }
 }
 
