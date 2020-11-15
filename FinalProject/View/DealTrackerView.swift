8f1f87ca93421863557e896da43bcbf61845b7dc
Patrick Ouellette <patrick.ouellette@hotmail.com>
2020-11-15 16:29:18 -0500

    created 2 views (DealTracker and StoreMap)

---
+++ FinalProject/View/DealTrackerView.swift
@@ -0,0 +1,20 @@
+//
+//  DealTrackerView.swift
+//  FinalProject
+//
+//  Created by Patrick Ouellette on 2020-11-15.
+//
+
+import SwiftUI
+
+struct DealTrackerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DealTrackerView_Previews: PreviewProvider {
+    static var previews: some View {
+        DealTrackerView()
+    }
+}
---
+++ FinalProject/View/StoreMapView.swift
@@ -0,0 +1,20 @@
+//
+//  StoreMapView.swift
+//  FinalProject
+//
+//  Created by Patrick Ouellette on 2020-11-15.
+//
+
+import SwiftUI
+
+struct StoreMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StoreMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoreMapView()
+    }
+}
