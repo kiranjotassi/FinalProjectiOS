cbbc61c5b89a7a1638557ddfb7f32a29f53372ab
Patrick Ouellette <patrick.ouellette@hotmail.com>
2020-11-17 11:51:07 -0500

    Created Deal Model

---
+++ FinalProject/Model/Deal.swift
@@ -0,0 +1,30 @@
+//
+//  Deal.swift
+//  FinalProject
+//
+//  Created by Patrick Ouellette on 2020-11-17.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Deal : Codable, Hashable {
+    @DocumentID var id = UUID().uuidString
+    var store: String = ""
+    var image: String = ""
+    var advertisedDeal: String = ""
+    var dealLocation: String = ""
+    var dealLat: Double = 0.0
+    var dealLng: Double = 0.0
+    
+    init(){}
+    
+    init(store: String, image: String, advertisedDeal: String, dealLocation: String){
+        
+        self.store = store
+        self.image = image
+        self.advertisedDeal = advertisedDeal
+        self.dealLocation = dealLocation
+    }
+}
